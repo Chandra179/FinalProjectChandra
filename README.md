924385e7f5a26b368bc84173ed4748b2d87dfc05
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-06-29 08:25:55 +0700

    'add'

---
+++ README.md
@@ -0,0 +1,15 @@
+## Database Migration (FLyWay) (PostgreSQL)
+
+Untuk migrasi database diwajibkan membuat tabel terlebih dahulu (DDL)
+	
+`pg_dump -U "db_user" -s db_bus`
+	
+1. dump database schema
+2. masukan schema kedalam file V1.0__ddl.sql
+3. masukan file V1.0__ddl.sql kedalam "src/main/resources/db/migration"
+4. Insert data
+5. buat file V1.1__data.sql
+6. isi file dengan INSERT(data)<br>
+
+
+## Database Migration (FLyWay) (PostgreSQL)
--- src/main/resources/db/migration/V1.1__data.sql
+++ src/main/resources/db/migration/V1.1__data.sql
@@ -1,14 +1,14 @@
-INSERT INTO tb_user(id, email, first_name, last_name, mobile_number, password, username)
-VALUES (1, 'chan@gmail.com','Chandra','aja','094727437','123445','chandra');
+INSERT INTO tb_user(email, first_name, last_name, mobile_number, password, username)
+VALUES ('chan@gmail.com','Chandra','aja','094727437','chan123','chan');
 
-INSERT INTO tb_agency(id, code, details, name, owner_user_id)
-VALUES (1, 'BDL','PT Angkasa Muda','Angkot Karang',1);
-
-INSERT INTO tb_bus(id, capacity, code, make, agency_id)
-VALUES (1, 20,'BDL01','20',1);
+INSERT INTO tb_agency(code, details, name, owner_user_id)
+VALUES ('BDL','PT Angkasa Muda','Angkot Karang',1);
 
 INSERT INTO tb_bus(capacity, code, make, agency_id)
-VALUES (2, 15,'BDL02','15',1);
+VALUES (20,'BDL01','20',1);
+
+INSERT INTO tb_bus(capacity, code, make, agency_id)
+VALUES (15,'BDL02','15',1);
 
 INSERT INTO tb_role (name) VALUES ('ROLE_USER');
 INSERT INTO tb_role (name) VALUES ('ROLE_ADMIN');
@@ -21,9 +21,9 @@ insert into tb_stop values
 (5, '1-10', 'Tanjung Senang', 'Jati Agung 1-10');
 
 INSERT INTO tb_trip VALUES
-(1, 15000, 15, 1, 1, 13, 1),
+(1, 15000, 15, 1, 1, 1, 3),
 (2, 15000, 2, 1, 1, 5, 2),
 (3, 15000, 3, 1, 1, 2, 3),
 (4, 15000, 5, 1, 1, 4, 4),
 (5, 15000, 4, 1, 1, 2, 5),
-(6, 15000, 7, 1, 1, 6, 6);
+(6, 15000, 7, 1, 1, 3, 5);
