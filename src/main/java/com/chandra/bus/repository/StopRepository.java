9f1f11beb97a2ce646cbcd0e653bd8f170beb758
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-07-02 07:14:09 +0700

    'add'

--- src/main/java/com/chandra/bus/repository/StopRepository.java
+++ src/main/java/com/chandra/bus/repository/StopRepository.java
@@ -3,10 +3,13 @@ package com.chandra.bus.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.chandra.bus.model.bus.Stop;
 
 public interface StopRepository extends JpaRepository<Stop, Long> {
+
+	@Query(value = "SELECT * FROM tb_stop t WHERE LOWER( t.name ) LIKE  %:name%", nativeQuery = true)
 	List<Stop> findByName(String name);
 
 	List<Stop> findByCode(String code);
