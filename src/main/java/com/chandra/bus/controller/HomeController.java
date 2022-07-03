8146d323b278561a1df7f5a4253f7d32c8111ecf
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-07-04 06:49:50 +0700

    add

--- src/main/java/com/chandra/bus/controller/HomeController.java
+++ src/main/java/com/chandra/bus/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.chandra.bus.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-	@RequestMapping("")
-	public String landing() {
-		return "redirect:/swagger-ui.html";
+	@GetMapping("/")
+	public RedirectView redirectWithUsingRedirectView() {
+		return new RedirectView("/swagger-ui.html");
 	}
 }
