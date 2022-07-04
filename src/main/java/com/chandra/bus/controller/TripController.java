751050599f5f75bcbed140ba913dde0e02fcca32
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-07-04 18:50:39 +0700

    add

--- src/main/java/com/chandra/bus/controller/TripController.java
+++ src/main/java/com/chandra/bus/controller/TripController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -140,6 +141,21 @@ public class TripController {
 			return ResponseHandler.generateResponse("No data found", HttpStatus.NOT_FOUND, trip);
 		}
 		return ResponseHandler.generateResponse("success", HttpStatus.OK, trip);
+	}
+
+	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@ApiOperation(value = "delete trip", authorizations = { @Authorization(value = "apiKey") })
+	public ResponseEntity<?> deleteStop(@PathVariable(value = "id") Long id) {
+
+		try {
+			tripRepository.deleteById(id);
+			String result = "Success Delete Trip with Id: " + id;
+			return ResponseEntity.ok(result);
+
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e.getCause());
+		}
 	}
 
 }
